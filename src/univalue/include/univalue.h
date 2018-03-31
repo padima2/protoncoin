aab3b2c21651b7568975868f3bbf59d5cf8c5d91
ProtonCoin <newprotoncoin@gmail.com>
2018-03-31 05:16:12 +0200

    Fixed some issues to build with Travis CI

--- src/univalue/include/univalue.h
+++ src/univalue/include/univalue.h
@@ -1,5 +1,5 @@
 // Copyright 2014 BitPay Inc.
-// Copyright 2015 Raven Core Developers
+// Copyright (c) 2017-2018 The Proton Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
